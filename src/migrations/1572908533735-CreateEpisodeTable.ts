b78f28d95d9c2a5c6be86d3bd98ee74b6f68e898
Vincent Schoener <vincent.schoener@gmail.com>
2019-11-05 00:17:19 +0100

    Create Episode table migration

---
+++ src/migrations/1572908533735-CreateEpisodeTable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateEpisodeTable1572908533735 implements MigrationInterface {
+  name = 'CreateEpisodeTable1572908533735';
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "episode" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "number" integer NOT NULL, "description" character varying NOT NULL, "status" character varying NOT NULL, CONSTRAINT "PK_7258b95d6d2bf7f621845a0e143" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "episode"`);
+  }
+}
