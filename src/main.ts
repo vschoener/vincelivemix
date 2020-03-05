6503722eb3fa0ed0116c49b0a90fb21dd5684422
Vincent Schoener <vincent.schoener@gmail.com>
2020-03-05 10:21:41 +0100

    Add migration command for production

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+
+// Shitty way to load the conf asap, need a config service
+dotenv.config();
+
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import { loggerSettings } from './core/logger/logger.settings';
 
 async function bootstrap() {
+  const logger = WinstonModule.createLogger(loggerSettings);
   const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger(loggerSettings),
+    logger
   });
-  await app.listen(3000);
+
+  // quick and dirty approach while getting a real config service
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Server running on port :${port}`);
 }
 bootstrap();
