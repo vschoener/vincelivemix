b20cc6e3efee450fbca9c76070b3bc3ca72972ee
Vincent Schoener <vincent.schoener@gmail.com>
2020-07-15 13:28:00 +0200

    Remove User and Auth module extracted to a new service (#132)

--- src/auth/dto/jwt.dto.ts
+++
@@ -1,5 +0,0 @@
-export type JwtDto = {
-  sub: number;
-  roles: string[];
-  isSuperAdmin: boolean;
-};
--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException } from '@nestjs/common';
 
 import { EpisodesController } from './episodes.controller';
 import { EpisodesService } from './episodes.service';
