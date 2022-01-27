b0ecde826c76da0a0c936c69909d540d210cd1e6
Vincent Schoener <vincent.schoener@gmail.com>
2022-01-27 01:42:16 +0100

    Switch APP_PORT to PORT

--- src/config/configs/web-server.config.ts
+++ src/config/configs/web-server.config.ts
@@ -1,5 +1,5 @@
 import { WebServerConfigDto } from '../dto/web-server-config.dto';
 
 export const webServerConfigDto: WebServerConfigDto = {
-  port: Number(process.env.APP_PORT) || 8080,
+  port: Number(process.env.PORT) || 8080,
 };
