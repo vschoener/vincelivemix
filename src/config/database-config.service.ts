740b5e2a6f4862c8d3722a5183ed8cc2ddc3acc6
Vincent Schoener <vincent.schoener@gmail.com>
2023-02-08 23:33:14 +0000

    Fix TypeORM change with DataSource config

--- src/config/database-config.service.ts
+++ src/config/database-config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 import { DatabaseConfigDto } from './dto/database-config.dto';
 
@@ -12,7 +13,7 @@ export class DatabaseConfigService {
    */
   public static getTypeORMConfig(
     config: DatabaseConfigDto,
-  ): TypeOrmModuleOptions {
+  ): TypeOrmModuleOptions & Partial<PostgresConnectionOptions> {
     return {
       keepConnectionAlive: true,
       name: 'default',
@@ -25,6 +26,32 @@ export class DatabaseConfigService {
       retryAttempts: config.retriesNumber,
       migrations: ['dist/migrations/**/*.{ts,js}'],
       autoLoadEntities: true,
+      ssl: config.ssl,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    };
+  }
+
+  /**
+   * We care having this used by CLI or App to keep one unique point of true
+   *
+   * @param config
+   */
+  public static getDataSourceConfig(
+    config: DatabaseConfigDto,
+  ): PostgresConnectionOptions {
+    return {
+      name: 'default',
+      type: 'postgres',
+      url: config.url,
+      synchronize: config.synchronize,
+      migrationsRun: config.autoRunMigration,
+      logging: config.logging,
+      logger: 'debug',
+      migrations: ['dist/migrations/**/*.{ts,js}'],
       ssl: config.ssl,
       extra: {
         ssl: {
--- src/config/typeorm-cli.config.ts
+++ src/config/typeorm-cli.config.ts
@@ -4,6 +4,7 @@ import { validateSync } from 'class-validator';
 import { DatabaseConfigDto } from './dto/database-config.dto';
 import { DatabaseConfigService } from './database-config.service';
 import { databaseConfigDto } from './configs/database-config';
+import { DataSource } from 'typeorm';
 
 /**
  * Handle TypeORM CLI Case
@@ -13,7 +14,9 @@ if (!module.parent) {
 }
 
 const config = new DatabaseConfigDto(databaseConfigDto);
+const dataSourceConfig = DatabaseConfigService.getDataSourceConfig(config)
 
 validateSync(config);
 
-export = DatabaseConfigService.getTypeORMConfig(config);
+const dataSource = new DataSource(dataSourceConfig)
+export default dataSource
